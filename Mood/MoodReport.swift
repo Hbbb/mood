e645903dba2b358ab01002ab92428026bc615c84
Harrison <hborges005@gmail.com>
2020-05-26 13:19:31 -0700

    Refactor saving into MoodReport

--- Mood/MoodReport.swift
+++ Mood/MoodReport.swift
@@ -18,4 +18,39 @@ struct MoodReport: Codable {
 		self.deviceID = deviceID
 		self.dateTime = Date()
 	}
+
+	func save(result: @escaping (Result<String, Error>) -> Void) {
+		let url = URL(string: "https://httpstat.us/422")
+		guard let requestUrl = url else { fatalError() }
+
+		var request = URLRequest(url: requestUrl)
+		request.httpMethod = "POST"
+
+		request.setValue("application/json", forHTTPHeaderField: "Accept")
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+		let json = try! JSONEncoder().encode(self)
+
+		request.httpBody = json
+
+		let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+			if let error = error {
+				return result(.failure(error))
+			}
+
+			guard let response = response as? HTTPURLResponse else {
+				let error = NSError(domain: "decoding response", code: 0, userInfo: nil)
+				return result(.failure(error))
+			}
+
+			guard response.statusCode == 201 else {
+				let error = NSError(domain: "save mood", code: 0, userInfo: nil)
+				return result(.failure(error))
+			}
+
+			return result(.success("Success"))
+		}
+
+		task.resume()
+	}
 }
